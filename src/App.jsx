bc7ed986b7084983c8d35615824695fe59ebd605
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-07-05 18:48:22 +0530

    PasswordGenerator

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState, useCallback, useEffect, useRef } from 'react'
+import './App.css'
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    <div className="comic-container">
+      <h1 className="comic-title">Password Generator</h1>
+      <div className="comic-input-container">
+        <input
+          type="text"
+          value={password}
+          className="comic-input"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button onClick={copyPasswordToClipboard} className="comic-button">
+          copy
+        </button>
+      </div>
+      <div className="comic-controls">
+        <div className="comic-slider">
+          <label>Length: {length}</label>
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="comic-range"
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </div>
+        <div className="comic-checkboxes">
+          <div>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => setNumberAllowed((prev) => !prev)}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => setCharAllowed((prev) => !prev)}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+}
+
+export default App
